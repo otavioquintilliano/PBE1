8f59a11d6d2923243a248a1a3b58bab7fdeaa29d
otavioquintilliano <159180588+otavioquintilliano@users.noreply.github.com>
2024-08-27 12:51:12 -0300

    Update Pokémon.java

--- ATIVIDADE03/Pokémon.java
+++ ATIVIDADE03/Pokémon.java
@@ -1,27 +1,45 @@
-package prjPokémon;
+package prjPokemon;
 
-public class Pokémon {
+public class Pokemon {
+	
 	//Atributos
-	String atributoNome;
-	String atributoTipo;
-	int atributoNivel;
-	int atributoHp;
 	
-	//Construtores
-	public void Pokémon() {
+			String atributoNome;
+			String atributoTipo;
+			int atributoNivel;
+			int atributoHp;
+			
+			//Construtores
+			
+			public Pokemon() {
+			}
+			
+			public Pokemon(String parametroNome, String parametroTipo, String parametroNivel, double parametroHP){
+				
+				this.atributoNome = parametroNome;
+				this.atributoTipo = parametroTipo;
+				this.atributoNivel = parametroNivel;
+				this.atributoHP = parametroHP;
+			}
+			
+			
+			//Metodos
+			
+			
+			public void metodoAtacar() {
+					System.out.println(this.atributoNome + " está atacando: " + this.atributoNome);
+				}
+			
+			public void metodoEvoluir() {
+				System.out.println(this.atributoNome + " está evoluindo!!");
+			}
+			
+	        public void exibirinfo() {
+		    	
+			    System.out.println("Nome: " + this.atributoNome);
+		    	System.out.println("Tipo: " + this.atributoTipo);
+		    	System.out.println("Nivel: " + this.atributoNivel);
+		    	System.out.println("Vida: " + this.atributoHP);
+				
+	        }
 	}
-	public void Pokémon(String parametroNome, String parametroTipo, int parametroNivel, int parametroHp) {
-		this.atributoNome = parametroNome;
-		this.atributoTipo = parametroTipo;
-		this.atributoNivel = parametroNivel;
-		this.atributoHp = parametroHp;
-	}
-	//Metodos
-	
-	public void metodoEvoluir {
-		System.out.println(this.atributoNome);
-	}
-	public void metodoAtacar {
-		System.out.println("Nome: " + this.atributoNome + "esta actacando");
-	}
-		
