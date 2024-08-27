c4afd2f0ff00b75a52ab9d7a91afb2c9baa70333
otavioquintilliano <159180588+otavioquintilliano@users.noreply.github.com>
2024-08-27 12:50:38 -0300

    Update Aplicacao.java

--- ATIVIDADE03/Aplicacao.java
+++ ATIVIDADE03/Aplicacao.java
@@ -1,10 +1,57 @@
-package prjPokémon;
+package prjPokemon;
 
 public class Aplicacao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Pokemon chamander = new Pokemon();
+		chamander.atributoNome = "charmander";
+		chamander.atributoTipo = "Fogo";
+		chamander.atributoNivel = 15;
+		chamander.atributoHp = 100;
+		
+		Pokemon squirtle = new Pokemon();
+		squirtle.atributoNome = "Squirtle";
+		squirtle.atributoTipo = "Água";
+		squirtle.atributoNivel = 14;
+		squirtle.atributoHp = 79;
+		
+		Pokemon pikachu = new Pokemon();
+		pikachu.atributoNome = "Pikachu";
+		pikachu.atributoTipo = "Elétrico";
+		pikachu.atributoNivel = 16;
+		pikachu.atributoHp = 97;
+	
+		Pokemon Pidgey = new Pokemon();
+		Pidgey.atributoNome = "Pidgey";
+		Pidgey.atributoTipo = "Voador";
+		Pidgey.atributoNivel = 10;
+		Pidgey.atributoHp = 99;
+		
+		Pokemon rattata = new Pokemon();
+		rattata.atributoNome = "rattata";
+		rattata.atributoTipo = "Normal";
+		rattata.atributoNivel = 17;
+		rattata.atributoHp = 96;
 
+		charmander.exibirinfo();
+		charmander.metodoAtacar();
+		charmander.metodoEvoluir();
+		
+		squirtle.exibirinfo();
+     		squirtle.metodoAtacar();
+		squirtle.metodoEvoluir();
+		
+		charmander.exibirinfo();
+		charmander.metodoAtacar();
+		charmander.metodoEvoluir();
+		
+		pikachu.exibirinfo();
+		pikachu.metodoAtacar();
+		pikachu.metodoEvoluir();
+		
+		rattata.exibirinfo();
+		rattata.metodoAtacar();
+		rattata.metodoEvoluir();
 	}
 
 }
