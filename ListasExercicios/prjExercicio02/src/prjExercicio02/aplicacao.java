5743d13239fa97ced24b3cdac5938549e90df1c3
otavioquintilliano <159180588+otavioquintilliano@users.noreply.github.com>
2024-09-17 13:01:06 -0300

    Add files via upload

---
+++ ListasExercicios/prjExercicio02/src/prjExercicio02/aplicacao.java
@@ -0,0 +1,10 @@
+package prjExercicio02;
+
+public class aplicacao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ListasExercicios/prjExercicio03/src/prjExercicio03/leao.java
@@ -0,0 +1,10 @@
+package prjExercicio03;
+
+public class leao extends Animal {
+
+	
+	//Metodos da Subclasse
+		public void metodoCacar() {
+			System.out.println(this.atributoNome + " está caçando!");
+		}
+}
