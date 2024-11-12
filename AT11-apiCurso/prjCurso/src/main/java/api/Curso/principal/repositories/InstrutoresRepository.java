1daa467d46658aa395a9b6d0c6ecddde75f8434b
otavioquintilliano <159180588+otavioquintilliano@users.noreply.github.com>
2024-11-12 07:41:04 -0300

    Delete AT11-apiCurso/prjCurso directory

--- AT11-apiCurso/prjCurso/src/main/java/api/Curso/principal/repositories/InstrutoresRepository.java
+++
@@ -1,9 +0,0 @@
-package api.Curso.principal.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import api.Curso.principal.entities.Instrutores;
-
-public interface InstrutoresRepository extends JpaRepository<Instrutores, Long> {
-
-}
